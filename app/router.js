cd4ce6176fb5341c2c96b2022d7de86bdd1b8b8a
Nathan Vranicar <nathan@Nathans-MacBook-Pro-4.local>
2017-06-05 17:03:48 -0400

    finished bio

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('bio');
+  this.route('contact');
+  this.route('resume');
+  this.route('portfolio');
 });
 
 export default Router;
